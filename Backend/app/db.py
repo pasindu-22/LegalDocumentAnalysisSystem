d986b5710b24c81b3d49d1b23ee789fe17626358
ilampoornan <ilampooornan@gmail.com>
2025-05-08 04:11:08 +0530

    added user register,login routes and get current user dependency

---
+++ Backend/app/db.py
@@ -0,0 +1,27 @@
+from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
+import models
+import os
+
+# Load environment variables
+load_dotenv()
+
+# Database URL from .env
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create engine
+engine = create_engine(DATABASE_URL, echo=True)
+
+# Dependency for FastAPI routes
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+# Function to create DB tables
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+# Only create tables if script is run directly
+if __name__ == "__main__":
+    create_db_and_tables()
+    print("✅ Tables created successfully.")
